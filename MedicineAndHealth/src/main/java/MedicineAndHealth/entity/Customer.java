044b26b6af783bbee2df3be085453c37e9d5b172
LLjun <zmj_private@163.com>
2018-05-27 18:58:53 +0800

    commit Customer.java

--- MedicineAndHealth/src/main/java/MedicineAndHealth/entity/Customer.java
+++ MedicineAndHealth/src/main/java/MedicineAndHealth/entity/Customer.java
@@ -1,5 +1,29 @@
 package MedicineAndHealth.entity;
 
 public class Customer {
-
+	private int customerId;
+	private String customerName;
+	private String password;
+	
+	public int getCustomerId() {
+		return customerId;
+	}	
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
