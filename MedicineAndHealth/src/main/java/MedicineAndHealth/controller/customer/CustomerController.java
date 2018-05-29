a6aa027af84c704358c2bd1d350e1a50fde24530
dqqdegethub <2452798461@qq.com>
2018-05-29 20:55:37 +0800

    123

--- MedicineAndHealth/src/main/java/MedicineAndHealth/controller/customer/CustomerController.java
+++ MedicineAndHealth/src/main/java/MedicineAndHealth/controller/customer/CustomerController.java
@@ -81,8 +81,7 @@ private final Logger LOG = LoggerFactory.getLogger(CustomerController.class);
 			
 		}
 		else {
-			//未注册过
-			
+			//未注册
 			int userId=cs.customerRegister(c);//
 			Customer c3=cs.queryCustomerById(userId);
 			response.setObj(c3);
